dfdde6a2c1d174008e16924eb0e2aceee4ca8613
Anton <kodvmeshke@gmail.com>
2022-07-30 16:29:53 +0300

    Block "Promise" made and adapted

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+	$('.slider').slick({
+		arrows: true,
+		infinite: true,
+		autoplay: true,
+		autoplaySpeed: 2000,
+		slidesToShow: 1,
+		adaptiveHeight: true
+	});
+});
